57728878a59137087546aa03be423c96e754abab
Nicholas Betito <nbetito@gmail.com>
2018-08-30 03:38:27 +0000

    automatically backed up by learn

--- lib/board.rb
+++ lib/board.rb
@@ -1,9 +1,2 @@
 # Define the variable board below.
-<<<<<<< HEAD
 board = [" "," "," "," "," "," "," "," "," "]
-=======
-board = ["row1","row2","row3"]
-row1 = [" ", " ", " "]
-row2 = [" ", " ", " "]
-row3 = [" ", " ", " "]
->>>>>>> 1441449e388d139a49029818ad958813b3bc01d4
